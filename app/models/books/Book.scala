ee058a76c9e02ed812c5c7b69975516915559adf
Mark Kubiak <markubiak@gmail.com>
2014-03-14 09:23:44 -0400

    Added function Book.addCoverByIsbn(isbn) and Book.AddCovers()

--- app/models/books/Book.scala
+++ app/models/books/Book.scala
@@ -1,11 +1,11 @@
 package models.books
 
 import config.users.UsesDataStore
-import scala.language.postfixOps
 import models.users.Permission
 import java.io.File
 import java.net.URL
 import sys.process._
+import javax.imageio.ImageIO
 
 class Book {
 
@@ -16,12 +16,20 @@ object Book extends UsesDataStore {
     val Manage = Permission(classOf[Book], 0, "Manage", "can manage textbook system")
     val LookUp = Permission(classOf[Book], 1, "LookUp", "can look up info about books and users")
   }
-  def addCoverByIsbn() = {
+  def addCovers() {
     val titles = dataStore.pm.query[Title].executeList.sortWith((c1, c2) => c1.name < c2.name)
     val isbns = titles.map(_.isbn)
     for (isbn <- isbns.take(40)) {
-      new URL("http://covers.openlbrary.org/b/isbn/" + isbn + "-S.jpg") #> new File("public/images/books/" + isbn + "-thumbnail.jpg") !!;
-      new URL("http://covers.openlbrary.org/b/isbn/" + isbn + "-L.jpg") #> new File("public/images/books/" + isbn + ".jpg") !!;
+      addCoverByIsbn(isbn)
     }
+  }
+  
+  def addCoverByIsbn(isbn: String) {
+    val thumbnailUrl = new URL(s"http://covers.openlibrary.org/b/isbn/${isbn}-S.jpg")
+    val bigUrl = new URL(s"http://covers.openlibrary.org/b/isbn/${isbn}-L.jpg")
+    val thumbnail = ImageIO.read(thumbnailUrl)
+    ImageIO.write(thumbnail, "jpg", new File(s"public/images/books/${isbn}-thumbnail.jpg"))
+    val bigimg = ImageIO.read(bigUrl)
+    ImageIO.write(bigimg, "jpg", new File(s"public/images/books/${isbn}.jpg"))
   }
 }
