54a7f7ebf0e728bb6c58eb59579f73b6298b6378
Mark Kubiak <mkubiak@gmail.com>
2014-04-24 10:31:32 -0400

    Fixed errors so that invalid URLs would not throw errors

--- app/models/books/Book.scala
+++ app/models/books/Book.scala
@@ -5,7 +5,8 @@ import models.users.Permission
 import java.io.File
 import java.net.URL
 import sys.process._
-import javax.imageio.ImageIO
+import javax.imageio._
+import java.io.IOException
 
 class Book {
 
@@ -25,11 +26,21 @@ object Book extends UsesDataStore {
   }
   
   def addCoverByIsbn(isbn: String) {
-    val thumbnailUrl = new URL(s"http://covers.openlibrary.org/b/isbn/${isbn}-S.jpg")
-    val bigUrl = new URL(s"http://covers.openlibrary.org/b/isbn/${isbn}-L.jpg")
-    val thumbnail = ImageIO.read(thumbnailUrl)
-    ImageIO.write(thumbnail, "jpg", new File(s"public/images/books/${isbn}-thumbnail.jpg"))
-    val bigimg = ImageIO.read(bigUrl)
-    ImageIO.write(bigimg, "jpg", new File(s"public/images/books/${isbn}.jpg"))
+    try {
+      val thumbnailUrl = new URL(s"http://covers.openlibrary.org/b/isbn/${isbn}-S.jpg?default=false")
+      val thumbnail = ImageIO.read(thumbnailUrl)
+      ImageIO.write(thumbnail, "jpg", new File(s"public/images/books/${isbn}-thumbnail.jpg"))
+    } catch {
+      case e: IOException => //Nothing
+      case e: IIOException => //Nothing
+    }
+    try {
+      val bigUrl = new URL(s"http://covers.openlibrary.org/b/isbn/${isbn}-L.jpg?default=false")
+      val bigimg = ImageIO.read(bigUrl)
+      ImageIO.write(bigimg, "jpg", new File(s"public/images/books/${isbn}.jpg"))
+    } catch {
+      case e: IOException => //Nothing
+      case e: IIOException => //Nothing
+    }
   }
 }
